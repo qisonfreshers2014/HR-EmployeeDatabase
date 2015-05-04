7c07ea5a643572375908f5eeebbd89fee9d79151
shelkerahul <rahul.shelke2@gmail.com>
2015-05-04 23:01:25 +0530

    Designation commiting

---
+++ src/java/main/com/hred/persistence/dao/DesignationTypeDAO.java
@@ -0,0 +1,8 @@
+package com.hred.persistence.dao;
+
+import com.hred.model.DesignationType;
+import com.hred.model.Employee;
+
+public interface DesignationTypeDAO {
+	public DesignationType getDesignationByID(Employee getDesignation);
+}
